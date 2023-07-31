af5c4d7c9e6201ba7c32926b1f2f859a8cbbffa5
onions41 <kblee89@outlook.com>
2023-07-30 21:04:10 -0700

    Comment out

--- DbAccess/ISqlDataAccess.cs
+++ DbAccess/ISqlDataAccess.cs
@@ -1,5 +1,5 @@
-public interface ISqlDataAccess {
-  Task<uint> InsertAndReturnId<T>(string statement, T parameters, string connectionId = "Default");
-  Task<IEnumerable<T>> LoadData<T, U>(string statement, U parameters, string connectionId = "Default");
-  Task SaveData<T>(string statement, T parameters, string connectionId = "Default");
-}
+// public interface ISqlDataAccess {
+//   Task<uint> InsertAndReturnId<T>(string statement, T parameters, string connectionId = "Default");
+//   Task<IEnumerable<T>> LoadData<T, U>(string statement, U parameters, string connectionId = "Default");
+//   Task SaveData<T>(string statement, T parameters, string connectionId = "Default");
+// }
--- DbAccess/SqlDataAccess.cs
+++ DbAccess/SqlDataAccess.cs
@@ -1,50 +1,50 @@
-using MySqlConnector;
-using Dapper;
-using Microsoft.Extensions.Configuration;
+// using MySqlConnector;
+// using Dapper;
+// using Microsoft.Extensions.Configuration;
 
-namespace DataAccess.DbAccess;
+// namespace DataAccess.DbAccess;
 
-public class SqlDataAccess : ISqlDataAccess {
-  private readonly IConfiguration _config;
-  public SqlDataAccess(IConfiguration config) {
-    _config = config;
-  }
+// public class SqlDataAccess : ISqlDataAccess {
+//   private readonly IConfiguration _config;
+//   public SqlDataAccess(IConfiguration config) {
+//     _config = config;
+//   }
 
-  // This gotta be async, so it doesn't lokc up the UI
-  // T is going to be one of the defined model type. Ie, the type this function returns
-  public async Task<IEnumerable<T>> LoadData<T, U>(
-    string statement,
-    U parameters,
-    string connectionId = "Default") // Name of the connection string, which I will have to initialize with user secrets
-  {
-    // Use the using so the connection closes properly even if something crashes
-    // This needs to be done whenever connecting to a database
-    using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
+//   // This gotta be async, so it doesn't lokc up the UI
+//   // T is going to be one of the defined model type. Ie, the type this function returns
+//   public async Task<IEnumerable<T>> LoadData<T, U>(
+//     string statement,
+//     U parameters,
+//     string connectionId = "Default") // Name of the connection string, which I will have to initialize with user secrets
+//   {
+//     // Use the using so the connection closes properly even if something crashes
+//     // This needs to be done whenever connecting to a database
+//     using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
 
-    // This is going ot return IEnumerable<T>
-    // Dapper extends the connection by providing these Query methods
-    return await connection.QueryAsync<T>(statement, parameters);
+//     // This is going ot return IEnumerable<T>
+//     // Dapper extends the connection by providing these Query methods
+//     return await connection.QueryAsync<T>(statement, parameters);
 
-    // When you come back https://youtu.be/dwMFg6uxQ0I?t=2985
-  }
+//     // When you come back https://youtu.be/dwMFg6uxQ0I?t=2985
+//   }
 
-  public async Task SaveData<T>(
-    string statement,
-    T parameters,
-    string connectionId = "Default"
-  ) {
-    using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
-    await connection.ExecuteAsync(statement, parameters);
+//   public async Task SaveData<T>(
+//     string statement,
+//     T parameters,
+//     string connectionId = "Default"
+//   ) {
+//     using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
+//     await connection.ExecuteAsync(statement, parameters);
 
-    // https://youtu.be/dwMFg6uxQ0I?t=3233
-  }
+//     // https://youtu.be/dwMFg6uxQ0I?t=3233
+//   }
 
-  public async Task<uint> InsertAndReturnId<T>(
-    string statement,
-    T parameters,
-    string connectionId = "Default"
-  ) {
-    using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
-    return await connection.QuerySingleAsync<uint>(statement, parameters);
-  }
-}
+//   public async Task<uint> InsertAndReturnId<T>(
+//     string statement,
+//     T parameters,
+//     string connectionId = "Default"
+//   ) {
+//     using MySqlConnection connection = new MySqlConnection(_config.GetConnectionString(connectionId));
+//     return await connection.QuerySingleAsync<uint>(statement, parameters);
+//   }
+// }
