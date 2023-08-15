0a0989196cf1bb4c8bf264837211e4d61a62d8af
onions41 <kblee89@outlook.com>
2023-08-14 23:52:33 -0700

    Inserting a Video

--- Data/IVideoData.cs
+++ Data/IVideoData.cs
@@ -1,3 +1,7 @@
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
 public interface IVideoData {
-  Task<uint> InsertVideo(VideoModel video);
+  Task<int> InsertVideo(VideoModel video);
 }
--- Data/VideoData.cs
+++ Data/VideoData.cs
@@ -5,26 +5,29 @@ using DataAccess.Mapping;
 
 namespace DataAccess.Data;
 
-public class VideoData : IVideoData {
-  private readonly IPostgresAccess _db;
+public class VideoData : IVideoData
+{
+   private readonly IPostgresAccess _db;
 
-  public VideoData(IPostgresAccess db) {
-    // DI
-    _db = db;
-    // Maps model property names to database column names
-    FluentMapper.Initialize(config => {
-      config.AddConvention<PascalToSnakeConvention>()
-        .ForEntity<MeetingModel>();
-    });
-  }
+   public VideoData(IPostgresAccess db) {
+      // DI
+      _db = db;
+      // Maps model property names to database column names
+      FluentMapper.Initialize(config => {
+         config.AddConvention<PascalToSnakeConvention>()
+            .ForEntity<MeetingModel>();
+      });
+   }
 
-  public async Task<uint> InsertVideo(VideoModel video) {
-    var statement = "INSERT INTO videos (url) VALUES (@Url) RETURNING videos.id";
+   public async Task<int> InsertVideo(VideoModel video) {
+      var statement = "INSERT INTO videos (url) VALUES (@Url) RETURNING videos.id";
 
-    return await _db.InsertAndReturnId(
-      statement,
-      new { Url = video.Url },
-      "CityClerkDb"
-    );
-  }
+      var videoId = await _db.InsertOne(
+         statement,
+         new { Url = video.Url },
+         "CityClerkDb"
+      );
+
+      return videoId ?? throw new Exception("Impossible. Inserting the video did not return a videoId");
+   }
 }
